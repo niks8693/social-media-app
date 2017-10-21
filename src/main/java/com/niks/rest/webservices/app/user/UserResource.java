e6115ff3f464947efdd78f53c7e7f4a883d0b88f
niks8693 <nikhil.shende@mavs.uta.edu>
2017-10-20 23:44:36 -0500

    added an exception to return correct status for resource not found error

--- src/main/java/com/niks/rest/webservices/app/user/UserResource.java
+++ src/main/java/com/niks/rest/webservices/app/user/UserResource.java
@@ -25,7 +25,11 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveOne(@PathVariable int id){
-		return service.findOne(id);
+		User user=service.findOne(id);
+		if(user==null){
+			throw new UserNotFoundException("id-"+id);
+		}
+		return user;
 	}
 	
 	@PostMapping("/users")
