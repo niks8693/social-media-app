6a557229cd2b1a2c926ca28356aa0f5bc500f807
niks8693 <nikhil.shende@mavs.uta.edu>
2017-10-20 22:29:29 -0500

    added User bean and UserDaoService

--- src/main/java/com/niks/rest/webservices/app/SocialMediaAppApplication.java
+++ src/main/java/com/niks/rest/webservices/app/SocialMediaAppApplication.java
@@ -1,4 +1,4 @@
-package com.niks.rest.webservices.socialmediaapp;
+package com.niks.rest.webservices.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
