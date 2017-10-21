ff31784227137ef27ef2fd4e2b50f32c208dfaa2
niks8693 <nikhil.shende@mavs.uta.edu>
2017-10-20 23:07:46 -0500

    added a post mapping to /users to create a user

--- src/main/java/com/niks/rest/webservices/app/user/UserResource.java
+++ src/main/java/com/niks/rest/webservices/app/user/UserResource.java
@@ -1,11 +1,16 @@
 package com.niks.rest.webservices.app.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -21,6 +26,16 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveOne(@PathVariable int id){
 		return service.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user){
+		User savedUser=service.save(user);
+		//We need to set Response status as CREATED
+		//We need to get back the URI we just created
+		//--> /users/4
+		URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
