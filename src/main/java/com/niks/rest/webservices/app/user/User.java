9fa40b6f6f098e40d732758df9b3cda344d684c4
niks8693 <nikhil.shende@mavs.uta.edu>
2017-10-21 21:51:40 -0500

    added javax persistence layer into the app

--- src/main/java/com/niks/rest/webservices/app/user/User.java
+++ src/main/java/com/niks/rest/webservices/app/user/User.java
@@ -2,14 +2,19 @@ package com.niks.rest.webservices.app.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 @ApiModel(description="Social Media App user with name, DOB as fields")
+@Entity
 public class User {
-	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min=2,message="User must have atleast 2 characters")
 	@ApiModelProperty(notes="Should be at least of 2 chars")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1,sysdate(),'KL Rahul');
+insert into user values(2,sysdate(),'Rohit Sharma');
+insert into user values(3,sysdate(),'Virat Kohli');
